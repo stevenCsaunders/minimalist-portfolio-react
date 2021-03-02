ae21d35aae38a9709aea8d35c6a9e74e0ed58da8
Steve <steve0183@gmail.com>
2021-03-02 01:14:53 -0700

    add styles for desktop contact us

--- src/Contact/ContactUs.js
+++ src/Contact/ContactUs.js
@@ -10,28 +10,30 @@ export const ContactUs = () => {
 		<article>
 			<ContactUsStyled>
 				<h2>Get in Touch</h2>
-				<p>
-					I’d love to hear about what you’re working on and how I can
-					help. I’m currently looking for a new role and am open to a
-					wide range of opportunities. My preference would be to find
-					a position in a company in London. But I’m also happy to
-					hear about opportunites that don’t fit that description. I’m
-					a hard-working and positive person who will always approach
-					each task with a sense of purpose and attention to detail.
-					Please do feel free to check out my online profiles below
-					and get in touch using the form.
-				</p>
-				<ul className='social-nav '>
-					<li>
-						<img src={github} alt='github' />
-					</li>
-					<li>
-						<img src={twitter} alt='twitter' />
-					</li>
-					<li>
-						<img src={linkedin} alt='linkedin' />
-					</li>
-				</ul>
+				<div>
+					<p>
+						I’d love to hear about what you’re working on and how I can
+						help. I’m currently looking for a new role and am open to a
+						wide range of opportunities. My preference would be to find
+						a position in a company in London. But I’m also happy to
+						hear about opportunites that don’t fit that description. I’m
+						a hard-working and positive person who will always approach
+						each task with a sense of purpose and attention to detail.
+						Please do feel free to check out my online profiles below
+						and get in touch using the form.
+					</p>
+					<ul className='social-nav '>
+						<li>
+							<img src={github} alt='github' />
+						</li>
+						<li>
+							<img src={twitter} alt='twitter' />
+						</li>
+						<li>
+							<img src={linkedin} alt='linkedin' />
+						</li>
+					</ul>
+				</div>
 			</ContactUsStyled>
 			<ContactForm />
 		</article>
